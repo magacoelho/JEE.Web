911f3aa3ca257dc8c9451eddbe59c8e78b34c9df
miw-upm <miw@eui.upm.es>
2015-02-14 14:33:46 +0100

    jsf. BasicBean

---
+++ src/main/java/es/art83/web/jsf/Entity.java
@@ -0,0 +1,13 @@
+package es.art83.web.jsf;
+
+public class Entity {
+    private String property;
+
+    public Entity() {
+        this.property = "Soy entidad";
+    }
+
+    public String getProperty() {
+        return property;
+    }
+}
